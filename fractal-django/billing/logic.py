79bd153d40854c1ba88d17bb2031dce4c47b2cef
Edson Ticona <xcr33d@gmail.com>
2019-06-01 23:28:27 +0200

    Add payment report and downloadable CSV file

--- fractal-django/billing/logic.py
+++ fractal-django/billing/logic.py
@@ -1,4 +1,4 @@
-from asistencia.models import Matricula, Student
+from asistencia.models import Matricula, Student, ApoderadoUser
 from .models import PaymentSettings, Payment
 from datetime import datetime
 
@@ -57,5 +57,44 @@ def getUserPaymentStatus(user):
         else:
           status = OVERDUE
       
-
   return status
+
+# This is just for the admin user!
+def getAdminReport():
+  total_amount = 0
+  total_paid_amount = 0
+  total_debt_amount = 0
+  payments = []         # by month
+  debtor_users = []     # Only considers the ones in OVERDUE state
+  not_configured_users_count = 0
+
+  today = datetime.now()
+  ps_count = 0
+  for ps in PaymentSettings.objects.all():
+    ps_count += 1
+    month_count = today.month-2
+    total_amount += month_count*ps.monthly_amount + ps.matricula_amount
+    if ps.has_promo:
+      total_amount -= ps.monthly_amount/2
+    # TODO: Optimize. One way could be to add a payment_state field 
+    # in the user table, so it's updated each time a payment is done and 
+    # by a cronjob every 1st and 15th of the month at 01:00 local time
+    user_status = getUserPaymentStatus(ps.matricula.student.apoderado)
+    if user_status == 3:
+        debtor_users.append({"username": ps.matricula.student.apoderado, "grado": ps.matricula.seccion} )
+    for p in Payment.objects.filter(payment_settings=ps).order_by('payment_settings__matricula__seccion', 'pay_reference'):
+      total_paid_amount += p.amount
+      payments.append({ "user": ps.matricula.student.apoderado.username, 
+                        "amount": p.amount, 
+                        "pay_reference": p.pay_reference, 
+                        "grado": ps.matricula.seccion
+                        })
+  not_configured_users_count = ApoderadoUser.objects.all().count() - ps_count
+  total_debt_amount = total_amount - total_paid_amount
+  return {
+          "total_debt_amount": total_debt_amount,
+          "payments"         : payments,
+          "debtors"          : debtor_users,
+          "no_config_users"  : not_configured_users_count,
+          "report_date"      : today.strftime("%Y-%m-%d")
+          }
